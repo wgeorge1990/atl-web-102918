96003834ea92e951969be854c100762367967779
kingcons <brit@kingcons.io>
2018-11-12 09:59:40 -0500

    Add starter code for migrations lecture.

---
+++ 11-active-record-migrations/config/environment.rb
@@ -0,0 +1,14 @@
+require 'date'
+require 'active_record'
+require 'bundler/setup'
+Bundler.require
+
+options = {
+  adapter: 'sqlite3',
+  database: 'db/store.db'
+}
+ActiveRecord::Base.establish_connection(options)
+
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+require_all 'app'
