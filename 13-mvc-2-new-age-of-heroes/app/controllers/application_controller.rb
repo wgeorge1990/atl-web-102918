9418f8210604ff44e0210c817da8c34cfabb29b6
kingcons <brit@kingcons.io>
2018-11-19 15:12:46 -0500

    Add support for finding a book and adding a new book.

--- 13-mvc-2-new-age-of-heroes/app/controllers/application_controller.rb
+++ 13-mvc-2-new-age-of-heroes/app/controllers/application_controller.rb
@@ -9,4 +9,19 @@ class ApplicationController < Sinatra::Base
     @books = Book.all
     erb :index
   end
+
+  get '/books/new' do
+    erb :new
+  end
+
+  get '/books/:id' do
+    id = params['id']
+    @book = Book.find(id)
+    erb :show
+  end
+
+  post '/books' do
+    @book = Book.create(params)
+    redirect "/books/#{@book.id}"
+  end
 end
