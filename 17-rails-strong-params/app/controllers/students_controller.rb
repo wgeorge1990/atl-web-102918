f4b149504334642935beefb21f3c41d20f1199fb
kingcons <brit@kingcons.io>
2018-11-27 11:22:36 -0500

    Add finished code from lecture.

--- 17-rails-strong-params/app/controllers/students_controller.rb
+++ 17-rails-strong-params/app/controllers/students_controller.rb
@@ -1,13 +1,31 @@
 class StudentsController < ApplicationController
   def new
+    @student = Student.new
+    @cohorts = Cohort.all
     render :new
   end
 
   def create
     @student = Student.new(name: params[:name],
                            cohort_id: params[:cohort_id])
-    @student.save
-    # byebug
+    if @student.save
+      redirect_to student_path(@student)
+    else
+      @message = "Your student couldn't be saved."
+      render :new
+    end
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+    @cohorts = Cohort.all
+    render :edit
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    byebug
+    @student.update(student_params)
     redirect_to student_path(@student)
   end
 
@@ -19,6 +37,10 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
     render :show
+  end
+
+  def student_params
+    params.permit(:name, :cohort_id)
   end
 
 end
--- 17-rails-strong-params/config/routes.rb
+++ 17-rails-strong-params/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :cohorts
-  resources :students, only: [:index, :show, :new, :create]
+  resources :students, except: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
