f9d298ec91322deade88269f1049a52893fb199f
kingcons <brit@kingcons.io>
2018-12-19 15:34:25 -0500

    Get the show panel working. Basically.

--- 27-organizing-without-classes/index.js
+++ 27-organizing-without-classes/index.js
@@ -6,7 +6,11 @@ function initPage() {
 
 // "View" function. just exists to turn an object -> html
 function bookView(book) {
-  return `<li class="book-item">${book.title}</li>`
+  return `<li id="book-${book.id}" class="book-item"
+              data-image="${book.img_url}"
+              data-description="${book.description}">
+            ${book.title}
+          </li>`
 }
 // "Model" function or fetch function. just exists to get the data
 function getBooks() {
@@ -17,15 +21,26 @@ function bookListView() {
   return getBooks().then(json => json.map(bookView).join(""))
 }
 
+function bookShowView(thumbnail, description, users) {
+  return `<div>
+            <img src="${thumbnail}" />
+            <p>${description}</p>
+            <p>Users who liked it: ${users}</p>
+          </div>`
+}
+
 function renderBookList() {
   let list = document.querySelector("#list")
   bookListView().then(booksHtml => {
       list.innerHTML = booksHtml
       let books = document.querySelectorAll(".book-item")
-      books.forEach(book => book.addEventListener('click', console.log))
+      books.forEach(book => book.addEventListener('click', renderBookShow))
   })
 }
 
-function renderBookShow() {
-  console.log("inside book show")
+function renderBookShow(event) {
+  const showPanel = document.querySelector("#show-panel")
+  let dataset = event.target.dataset
+  let html = bookShowView(dataset.image, dataset.description, [])
+  showPanel.innerHTML = html
 }
