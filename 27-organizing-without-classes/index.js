c78b7c7273a4512309eb49bb5af6188777d5117f
kingcons <brit@kingcons.io>
2018-12-19 15:20:23 -0500

    Refactor to add event listeners to books.

--- 27-organizing-without-classes/index.js
+++ 27-organizing-without-classes/index.js
@@ -1,21 +1,31 @@
-document.addEventListener("DOMContentLoaded", renderBookList);
+document.addEventListener("DOMContentLoaded", initPage);
 
+function initPage() {
+  renderBookList()
+}
+
+// "View" function. just exists to turn an object -> html
+function bookView(book) {
+  return `<li class="book-item">${book.title}</li>`
+}
 // "Model" function or fetch function. just exists to get the data
 function getBooks() {
   return fetch("http://localhost:3000/books").then(res => res.json())
 }
 
-// "View" function. just exists to turn an object -> html
-function bookListView(book) {
-  return `<li>${book.title}</li>`
+function bookListView() {
+  return getBooks().then(json => json.map(bookView).join(""))
 }
 
-function renderBookList () {
-  const list = document.querySelector("#list")
-  getBooks().then(json => {
-    json.forEach(book => {
-      let html = bookListView(book)
-      list.innerHTML += html
-    })
+function renderBookList() {
+  let list = document.querySelector("#list")
+  bookListView().then(booksHtml => {
+      list.innerHTML = booksHtml
+      let books = document.querySelectorAll(".book-item")
+      books.forEach(book => book.addEventListener('click', console.log))
   })
+}
+
+function renderBookShow() {
+  console.log("inside book show")
 }
